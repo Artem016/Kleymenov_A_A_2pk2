950f7498a5845f0dd8dda5880a3d00c5dc8997a2
Artem016 <114384373+Artem016@users.noreply.github.com>
2022-11-07 06:54:17 +0500

    Add files via upload

---
+++ pz10_/pz10_/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userText = Console.ReadLine();
+            string[] userTextArray = userText.Split();
+            int counter = 0;
+            for (int i = 0; i < userText.Split().Length; i++)
+            {
+                if (userTextArray[i][0] == 'С' || userTextArray[i][0] == 'с')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine("Слов которые начинаются с буквы C: " + counter);
+        }
+
+    }
+}
