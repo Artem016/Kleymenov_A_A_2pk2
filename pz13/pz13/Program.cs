308bc24163fdc1a316234af631b3f57e6d79761c
Artem016 <114384373+Artem016@users.noreply.github.com>
2022-11-16 10:38:20 +0500

    pz13
    pz13

---
+++ pz13/pz13/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace pz13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] myMass = new int[10];
+            for (int i = 0; i < myMass.Length; i++)
+            {
+                myMass[i] = rnd.Next(0, 25);
+                Console.WriteLine($"Элементе массива номер {i} равен: {myMass[i]}");
+            }
+            Console.WriteLine(SummEvenNumberedValua(myMass)); ;
+            
+        }
+
+        static int SummEvenNumberedValua(int[] myMass)
+        {
+            int summ = 0;
+            for (int i = 0; i < myMass.Length; i++)
+            {
+                if (myMass[i]%2 == 0 && i % 2 != 0)
+                {
+                    summ += myMass[i]; 
+                }
+            }
+            return summ;
+        }
+    }
+}
