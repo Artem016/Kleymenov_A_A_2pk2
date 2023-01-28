3c869900a93aeb64e12d3f5bc6ee155932cae356
NecoZero <114384373+Artem016@users.noreply.github.com>
2023-01-28 13:06:49 +0500

    Add files via upload

---
+++ Distant28.01/Distant28.01/ComplexNumber(a+bi).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distant28._01
+{
+    internal class ComplexNumber
+    {
+        public int a;//действительная часть
+        public int b;//мнимая часть
+        public const char i = 'i';//констнта
+        public static void PrintComplexNumber(ComplexNumber c_num)
+        {
+            Console.WriteLine($"Число: {c_num.a} + {c_num.b}{i}");
+        }
+
+        public static ComplexNumber GetMultiplicationByNumber(int natural_num, ComplexNumber c_num)
+        {
+            ComplexNumber time_c_num = new ComplexNumber();
+            time_c_num.a = c_num.a * natural_num;
+            time_c_num.b = c_num.b * natural_num;
+            return time_c_num;
+        }
+    }
+}
---
+++ Distant28.01/Distant28.01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Distant28._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber c_num1 = new ComplexNumber();
+            c_num1.a = 5;
+            c_num1.b = 23;
+            ComplexNumber.PrintComplexNumber(c_num1);
+            ComplexNumber c_num2 = ComplexNumber.GetMultiplicationByNumber(5, c_num1);
+            ComplexNumber.PrintComplexNumber(c_num2);
+        }
+    }
+}
