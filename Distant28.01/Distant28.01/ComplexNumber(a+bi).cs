2b4a266fb105a19e81a545ef14b64866131dbc00
NecoZero <114384373+Artem016@users.noreply.github.com>
2023-02-09 12:23:43 +0500

    Add files via upload

--- Distant28.01/Distant28.01/ComplexNumber(a+bi).cs
+++ Distant28.01/Distant28.01/ComplexNumber(a+bi).cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Проверять корректность вводимых данных
+//Подсчитывает общую сумму созданных объектов комплексных чисел. Общее количество объектов
+
 namespace Distant28._01
 {
     internal class ComplexNumber
@@ -11,6 +14,22 @@ namespace Distant28._01
         public int a;//действительная часть
         public int b;//мнимая часть
         public const char i = 'i';//констнта
+        public static int count = 0;
+
+        public ComplexNumber()
+        {
+            this.a = 0;
+            this.b = 0;
+            count++;
+        }
+
+        public ComplexNumber(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            count++;
+        }
+        
         public static void PrintComplexNumber(ComplexNumber c_num)
         {
             Console.WriteLine($"Число: {c_num.a} + {c_num.b}{i}");
@@ -22,6 +41,49 @@ namespace Distant28._01
             time_c_num.a = c_num.a * natural_num;
             time_c_num.b = c_num.b * natural_num;
             return time_c_num;
+        }
+
+        public static void ChangeComplexNumber(ComplexNumber userCNumber)
+        {
+            int time_a = 0;
+            int time_b = 0;
+            bool flag = true;
+
+            Console.Write("Введите значение действительной части: ");
+            while (flag)
+            {
+                try
+                {
+                    time_a = int.Parse(Console.ReadLine());
+                    flag = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Некорректное значение действительной части.");
+                }
+            }
+            Console.Write("Введите значение мнимой части: ");
+            flag = true;
+            while (flag)
+            {
+                try
+                {
+                    time_b = int.Parse(Console.ReadLine());
+                    flag = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Некорректное значение мнимой части.");
+                }
+            }
+            userCNumber.a = time_a;
+            userCNumber.b = time_b;
+             
+        }
+
+        public static int GetAmountComplexNumber()
+        {
+            return count;
         }
     }
 }
--- Distant28.01/Distant28.01/Program.cs
+++ Distant28.01/Distant28.01/Program.cs
@@ -3,13 +3,20 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {           
             ComplexNumber c_num1 = new ComplexNumber();
             c_num1.a = 5;
             c_num1.b = 23;
+
+            ComplexNumber c_num2 = new ComplexNumber(3, 13);
+
+            ComplexNumber.ChangeComplexNumber(c_num1);
             ComplexNumber.PrintComplexNumber(c_num1);
-            ComplexNumber c_num2 = ComplexNumber.GetMultiplicationByNumber(5, c_num1);
+
+            ComplexNumber c_num3 = ComplexNumber.GetMultiplicationByNumber(5, c_num1);
             ComplexNumber.PrintComplexNumber(c_num2);
+
+            Console.WriteLine($"Количестов созданных комплесных чисел: {ComplexNumber.GetAmountComplexNumber()}");
         }
     }
 }
