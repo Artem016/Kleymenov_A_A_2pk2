2d1b847b26d95f66288a6194116236a5be7e65a5
Artem016 <artemklemonow@gmail.com>
2023-03-06 08:20:33 +0500

    Загружена 24я практика по Программированию

---
+++ Distant28.01/ConsoleApp1/ComplexNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz24
+{
+    internal class ComplexNumber : ICloneable
+    {
+        const char i = 'i';
+        public int a;
+        public int b;
+
+        public ComplexNumber()
+        {
+            a = 0;
+            b = 0;
+            Console.WriteLine($"Комплексное число: {a} + {b}{i}");
+        }
+        public ComplexNumber(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            Console.WriteLine($"Комплексное число: {a} + {b}{i}");
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ Distant28.01/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber cm1 = new ComplexNumber();
+            ComplexNumber cm2 = new ComplexNumber(3, 4);
+            cm1 = (ComplexNumber)cm2.Clone();
+            Console.WriteLine(cm1.a);
+            Console.ReadLine();
+        }
+    }
+}
