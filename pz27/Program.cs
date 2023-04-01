5bf770361ed0c021a4ee16e7c9fa30525b96b2f5
NecoZero <114384373+Artem016@users.noreply.github.com>
2023-04-01 13:13:19 +0500

    Add files via upload

---
+++ pz27/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] stud1 = new Student[2];
+
+            for (int i = 0; i < stud1.Length; i++)
+            {
+                Console.WriteLine($"Введите данные для студена {i+1}...");
+                Console.Write("name: "); stud1[i].name = Console.ReadLine();
+                Console.Write("group: "); stud1[i].group = Console.ReadLine();
+                Console.Write("количество оценок: "); stud1[i].ses = new byte[byte.Parse(Console.ReadLine())];
+                for (int j = 0; j < stud1[i].ses.Length; j++)
+                {
+                    Console.Write($"оценка {j + 1}: "); stud1[i].ses[j] = Convert.ToByte(Console.ReadLine());
+                }
+            }
+            Student.PrintBudStudent(stud1);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pz27/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz27
+{
+    internal struct Student
+    {
+        public string name;
+        public string group;
+        public byte[] ses;
+        public static void PrintBudStudent(Student[] student)
+        {
+            bool isBudStudent = false;
+            Console.WriteLine("Фамилии студентов у которых есть оценки 2 или ниже:");
+            for (int i = 0; i < student.Length; i++)
+            {
+                for (int j = 0; j < student[i].ses.Length; j++)
+                {
+                    if (student[i].ses[j] <= 2)
+                    {
+                        Console.WriteLine(student[i].name);
+                        isBudStudent = true;
+                        break;
+                    }
+                }
+            }
+            if (!isBudStudent)
+            {
+                Console.WriteLine("Все студенты молодцы...");
+            }
+        }
+    }
+}
